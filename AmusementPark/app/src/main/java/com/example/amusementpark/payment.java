dff82c55ab04eef4dd2e88c0986905c3336aeb95
Lixuan Luo <lixuanluo@icloud.com>
2019-12-26 17:45:51 -0500

    update payment UI

--- AmusementPark/app/src/main/java/com/example/amusementpark/payment.java
+++ AmusementPark/app/src/main/java/com/example/amusementpark/payment.java
@@ -2,8 +2,10 @@ package com.example.amusementpark;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,7 @@ import androidx.core.content.ContextCompat;
 
 import com.baoyachi.stepview.HorizontalStepView;
 import com.baoyachi.stepview.bean.StepBean;
+import com.braintreepayments.cardform.view.CardForm;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,12 +21,13 @@ import java.util.List;
 public class payment extends AppCompatActivity {
     private Button confirm_bt;
 
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.payment);
 
-        /* Add Step Progress Bar*/
+        /* Import Step Progress Bar*/
         HorizontalStepView setpview = (HorizontalStepView) findViewById(R.id.step_view2);
         List<StepBean> stepsBeanList = new ArrayList<>();
         StepBean stepBean0 = new StepBean("Summary",1);
@@ -45,15 +49,38 @@ public class payment extends AppCompatActivity {
                 .setStepsViewIndicatorDefaultIcon(ContextCompat.getDrawable(payment.this, R.drawable.default_icon))//设置StepsViewIndicator DefaultIcon
                 .setStepsViewIndicatorAttentionIcon(ContextCompat.getDrawable(payment.this, R.drawable.attention));//设置StepsViewIndicator AttentionIcon
 
+
+
+
+        /*Import Card Form */
+        final CardForm cardForm = (CardForm) findViewById(R.id.card_form);
+        cardForm.cardRequired(true)
+                .expirationRequired(true)
+                .cvvRequired(true)
+                .cardholderName(CardForm.FIELD_REQUIRED)
+                .postalCodeRequired(true)
+                .mobileNumberRequired(true)
+                .mobileNumberExplanation("SMS is required on this number")
+                .actionLabel("Purchase")
+                .setup(payment.this);
+
+        cardForm.getCvvEditText().setInputType(InputType.TYPE_CLASS_NUMBER|InputType.TYPE_NUMBER_VARIATION_PASSWORD);
+
         /* Confirm Button Function*/
         confirm_bt = (Button) findViewById(R.id.next_bt2);
-        //Switch to the next page
+        //Switch to the next page if the form is completed
         confirm_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(payment.this,paymentDone.class);
-                startActivity(intent);
+
+                if(cardForm.isValid()){
+                    Intent intent = new Intent();
+                    intent.setClass(payment.this,paymentDone.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(payment.this,"Please complete the form",Toast.LENGTH_LONG).show();
+                }
+
             }
         });
     }
