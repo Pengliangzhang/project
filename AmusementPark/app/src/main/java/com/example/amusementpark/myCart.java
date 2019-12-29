84e550fd904892f43e10f61c679deec21f702c4b
Lixuan Luo <lixuanluo@icloud.com>
2019-12-28 21:53:50 -0500

    add list view in my cart

--- AmusementPark/app/src/main/java/com/example/amusementpark/myCart.java
+++ AmusementPark/app/src/main/java/com/example/amusementpark/myCart.java
@@ -3,7 +3,15 @@ package com.example.amusementpark;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,11 +27,20 @@ import java.util.List;
 public class myCart extends AppCompatActivity {
 
     private Button next_btn;
+    int[] image = {R.drawable.ticket,R.drawable.ticket};
+    String[] type = {"Student","Adult"};
+    String[] price = {"$15","$20"};
+    String[] remove ={"remove","remove"};
+
+
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_cart);
+
 
         /* Add Step Progress Bar*/
         HorizontalStepView setpview = (HorizontalStepView) findViewById(R.id.step_view);
@@ -47,6 +64,14 @@ public class myCart extends AppCompatActivity {
                 .setStepsViewIndicatorDefaultIcon(ContextCompat.getDrawable(myCart.this, R.drawable.default_icon))//设置StepsViewIndicator DefaultIcon
                 .setStepsViewIndicatorAttentionIcon(ContextCompat.getDrawable(myCart.this, R.drawable.attention));//设置StepsViewIndicator AttentionIcon
 
+
+
+        /* Add List view in myCart*/
+        ListView myListView = (ListView) findViewById(R.id.mycart_listview);
+        cartAdapter adapter = new cartAdapter();
+        myListView.setAdapter(adapter);
+
+
         /* Next Step Button Function*/
         //Initialize Button
         next_btn = (Button) findViewById(R.id.next_bt1);
@@ -62,4 +87,53 @@ public class myCart extends AppCompatActivity {
         });
 
     }
+
+    /*
+     * list view adapter for cart_adapter_view.xml
+     */
+    class cartAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return image.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = getLayoutInflater().inflate(R.layout.cart_adapter_view,null);
+
+            //Initialization
+            TextView t_type = (TextView) convertView.findViewById(R.id.listview_type);
+            TextView t_price = (TextView) convertView.findViewById(R.id.listview_price);
+            TextView t_remove = (TextView) convertView.findViewById(R.id.listview_remove);
+            Spinner t_numSpinner = (Spinner) convertView.findViewById(R.id.listview_num);
+            ImageView t_image = (ImageView) convertView.findViewById(R.id.listview_image);
+
+            //Set ticket numbers adapter
+            ArrayAdapter<String> ticketNumAdapter = new ArrayAdapter<String>(
+                    myCart.this,
+                    android.R.layout.simple_list_item_1,
+                    getResources().getStringArray(R.array.NumOfTickets));
+
+            //Set up each info.
+            t_type.setText(type[position]);
+            t_price.setText(price[position]);
+            t_remove.setText(remove[position]);
+            t_numSpinner.setAdapter(ticketNumAdapter);
+            t_image.setImageResource(image[position]);
+
+            return convertView;
+        }
+    }
+
 }
