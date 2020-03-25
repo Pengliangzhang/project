484327a81c8f29aedc22dfd8a5ed3012a61b4350
Tongdan <tongdan.zhu@carleton.ca>
2020-03-24 23:09:32 -0400

    automatic get plate number

--- AmusementPark/app/src/main/java/com/example/amusementpark/parking.java
+++ AmusementPark/app/src/main/java/com/example/amusementpark/parking.java
@@ -26,10 +26,12 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 
 public class parking extends AppCompatActivity {
@@ -46,6 +48,8 @@ public class parking extends AppCompatActivity {
     private static final int REQUEST_CODE_QR_SCAN = 101;
     private static final int SUCCESS = 1;
     private static final int FAIL = 0;
+    private static final int INITIAL=2;
+    private static final int INITIALFAIL=3;
     private boolean PASS = false;
     private TextView tv_message;
     private String message;
@@ -74,6 +78,16 @@ public class parking extends AppCompatActivity {
                     toast("failure");
 
                     PASS = false;
+                    break;
+                case INITIAL:
+                    if (msg.obj!=null){
+                        bt_confirm.setText("update");
+                        et_plate_number.setText(msg.obj.toString());
+                    }
+
+                    break;
+                case INITIALFAIL:
+                    bt_confirm.setText("confirm");
                     break;
 
             }
@@ -195,16 +209,91 @@ public class parking extends AppCompatActivity {
             }
         });
 
+        // initial plate number edit text
+        getPlateNumber();
+
     }
 
-    private void updatePlateNumber(final String username, final String plateNumber) {
-        int result = 0;
+    private void getPlateNumber() {
 
         new Thread(new Runnable() {
             @Override
             public void run() {
                 HttpURLConnection connection = null;
                 BufferedReader reader = null;
+                Message mess = handler.obtainMessage();
+                try {
+
+                    myConnection urlbase = new myConnection();
+                    String surl = urlbase.getUrl() + "queryparkingspot";
+                    System.out.println("surl " + surl);
+                    URL url = new URL(surl);
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.addRequestProperty("Cookie", sessionID);
+                    connection.setRequestMethod("GET");//设置连接超时时间（毫秒）
+                    connection.setConnectTimeout(5000);//设置读取超时时间（毫秒）
+                    connection.setReadTimeout(5000);//返回输入流
+                    InputStream in = connection.getInputStream();
+
+                    //读取输入流
+                    reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+                    System.out.println("==result: "+result);
+                    String [] temp;
+                    temp=result.toString().split(",");
+
+                    for(int i =0; i < temp.length ; i++){
+                        System.out.println(temp[i]);
+                        System.out.println("");
+                    }
+
+                    System.out.println("last item: "+temp[temp.length-1]);
+
+                    //get last plate number
+                    if (temp[temp.length-1].length()>13) {
+                        String[] getLastPlateNum = temp[temp.length - 1].split(":");
+                        int length = getLastPlateNum[getLastPlateNum.length - 1].length();
+                        String lastPlateNum = getLastPlateNum[getLastPlateNum.length - 1].substring(1, length - 4);
+
+                        System.out.println("last plate number: " + lastPlateNum);
+
+
+                        mess.what = INITIAL;
+                        mess.obj = lastPlateNum;
+                    }
+                    else{
+                        mess.what=INITIALFAIL;
+                    }
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+                }
+                handler.sendMessage(mess);
+
+            }
+        }).start();
+
+
+    }
+
+
+
+    private void updatePlateNumber(final String username, final String plateNumber) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
                 Message message = handler.obtainMessage();
                 try {
 
@@ -213,7 +302,6 @@ public class parking extends AppCompatActivity {
                     System.out.println("surl " + surl);
                     URL url = new URL(surl);
                     connection = (HttpURLConnection) url.openConnection();
-
 
                     connection.setDoOutput(true);// 设置是否向httpUrlConnection输出，因为这个是post请求，参数要放在; http正文内，因此需要设为true, 默认情况下是false;
                     connection.setDoInput(true);// 设置是否从httpUrlConnection读入，默认情况下是true;
@@ -233,6 +321,7 @@ public class parking extends AppCompatActivity {
                     BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream(), "UTF-8"));
                     writer.write(String.valueOf(jsonObject));
                     writer.close();
+
 
                     int responseCode = connection.getResponseCode();
                     if (responseCode == HttpURLConnection.HTTP_OK) {
